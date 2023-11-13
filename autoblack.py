b701183962087b0ac50121b2718fc278cbba131a
Marek Czernek <marek.czernek@suse.com>
2023-11-13 10:20:30 +0100

    Add init cmd line options

--- autoblack.py
+++ autoblack.py
@@ -1,24 +1,64 @@
 #!/bin/env python3
+"""
+This application executes black on a file,
+then pylint on the file,
+and finally, auto-disables all issues that
+pylint reports on that file.
+"""
+import click
 import subprocess
 
 FILE = "./yum_src.py"
 PYLINTRC = "./pylintrc"
 
-def main():
-    output = None
-    try:
-        output = subprocess.check_output([
-            "pylint",
-            "--msg-template='{line:3d}:{symbol}'",
-            "--rcfile",
-            PYLINTRC,
-            FILE
-        ], stderr=subprocess.STDOUT, text=True)
-    except subprocess.CalledProcessError as e:
-        output = e.output
 
-    pylint_map = parse_pylint_out(output)
+@click.version_option("0.1.0", prog_name="autoblack")
+@click.command("main")
+@click.option("-p", "--pylintrc", "pylintrc", type=click.File(mode="r"), default=None)
+@click.option(
+    "-t",
+    "--target-version",
+    "target_version",
+    type=click.Choice(
+        [
+            "py33",
+            "py34",
+            "py35",
+            "py36",
+            "py37",
+            "py38",
+            "py39",
+            "py310",
+            "py311",
+            "py312",
+        ]
+    ),
+)
+def main(pylintrc, target_version):
+    execute_black(target_version)
+    pylint_output = get_pylint_output(pylintrc)
+    pylint_map = parse_pylint_out(pylint_output)
     autofix_file(pylint_map)
+
+
+def get_pylint_output(pylintrc):
+    try:
+        output = subprocess.check_output(
+            [
+                "pylint",
+                "--msg-template='{line:3d}:{symbol}'",
+                "--rcfile",
+                PYLINTRC,
+                FILE,
+            ],
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+    except subprocess.CalledProcessError as e:
+        # when pylint finds errors, it returns non-zero code
+        output = e.output
+    return output
+
 
 def autofix_file(pylint_map):
     if not bool(pylint_map):
@@ -28,11 +68,14 @@ def autofix_file(pylint_map):
         for i, line in enumerate(lines):
             line_num = str(i + 1)
             if line_num in pylint_map:
-                new_line = f"{line.rstrip()} #  pylint: disable={pylint_map[line_num]}\n"
+                new_line = (
+                    f"{line.rstrip()} #  pylint: disable={pylint_map[line_num]}\n"
+                )
                 lines[i] = new_line
         f.seek(0)
         for line in lines:
             f.write(line)
+
 
 def parse_pylint_out(pylint_out):
     res = {}
@@ -47,13 +90,12 @@ def parse_pylint_out(pylint_out):
     return res
 
 
-
-def execute_cmd(cmd, out_f):
+def execute_black(target_version):
+    cmd = ["black", FILE]
+    if target_version:
+        cmd.extend(["-t", target_version])
     return subprocess.Popen(
-        cmd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=out_f
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
 
 if __name__ == "__main__":
