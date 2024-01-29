4605826dc4556991b6b2bae1e6cb4cc3b6eb9a83
Marek Czernek <marek.czernek@suse.com>
2024-01-29 14:12:53 +0100

    WIP: Add module functions

--- autolint.py
+++ autolint.py
@@ -56,6 +56,18 @@ def main(paths, pylintrc, target_version):
         pylint_output = get_pylint_output(pylintrc, file)
         pylint_map = parse_pylint_out(pylint_output)
         autofix_file(pylint_map, file)
+    
+#    execute_black(paths, target_version)
+
+    #files = recursively_parse_files(paths)
+    # dirs = ' '.join(paths)
+    # #import debugpy
+    # #debugpy.listen(("0.0.0.0", 5678))
+    # #debugpy.wait_for_client()
+    # # for file in files:
+    # pylint_output = get_pylint_output(pylintrc, dirs)
+    # pylint_map = parse_pylint_out_module(pylint_output)
+    # autofix_module(pylint_map)
 
 
 def recursively_parse_files(paths):
@@ -72,9 +84,17 @@ def get_pylint_output(pylintrc, file):
     cmd = [
                 "pylint",
                 "--msg-template='{line:3d}:{symbol}'",
-                f"--rcfile {pylintrc}" if pylintrc else "",
-                file,
+                "--rcfile",
+                "/home/mczernek/.pylintrc",
+                str(file),
             ]
+    # cmd = [
+    #             "pylint",
+    #             "--msg-template='{abspath}:{line:3d}:{symbol}'",
+    #             "--rcfile",
+    #             "/home/mczernek/.pylintrc",
+    #             str(file),
+    #         ]
     try:
         output = subprocess.check_output(
             cmd,
@@ -90,6 +110,57 @@ def get_pylint_output(pylintrc, file):
 def autofix_file(pylint_map, file):
     if not bool(pylint_map):
         print(f"Empty map for {file}, returning")
+        return
+    try:
+        with open(file, "r+", encoding="ISO-8859-1") as f:
+            lines = f.readlines()
+            for line_num,pylint_val in sorted(pylint_map.items(), key=lambda kv:int(kv[0]), reverse=True):
+                # arrays are 0-indexed, pylint-out is 1-indexed
+                line_num_i = int(line_num) - 1
+                curr_line = lines[line_num_i]
+                num_of_spaces = len(curr_line) - len(curr_line.lstrip())
+                new_line = ''.rjust(num_of_spaces)
+                new_line = f"{new_line}#  pylint: disable-next={pylint_val}\n"
+                if line_num_i == 0:
+                    new_line = new_line.replace("-next", "")
+                lines.insert(line_num_i, new_line)
+            f.seek(0)
+            for line in lines:
+                f.write(line)
+    except Exception as e:
+        print(f"Could not fix {file}")
+        print(e)
+
+def autofix_module(pylint_map):
+    if not bool(pylint_map):
+        print(f"Empty map, returning")
+        return
+    for file in pylint_map.keys():
+        print(f"Fixing {file}")
+        pylint_map_file = pylint_map[file]
+        try:
+            with open(file, "r+", encoding="UTF-8") as f:
+                lines = f.readlines()
+                for line_num,pylint_val in sorted(pylint_map_file.items(), key=lambda kv:int(kv[0]), reverse=True):
+                    # arrays are 0-indexed, pylint-out is 1-indexed
+                    line_num_i = int(line_num) - 1
+                    curr_line = lines[line_num_i]
+                    num_of_spaces = len(curr_line) - len(curr_line.lstrip())
+                    new_line = ''.rjust(num_of_spaces)
+                    new_line = f"{new_line}#  pylint: disable-next={pylint_val}\n"
+                    if line_num_i == 0:
+                        new_line = new_line.replace("-next", "")
+                    lines.insert(line_num_i, new_line)
+                f.seek(0)
+                for line in lines:
+                    f.write(line)
+        except Exception as e:
+            print(f"Could not fix {file}")
+            print(e)
+
+def autofix_files(pylint_map, file):
+    if not bool(pylint_map):
+        print(f"Empty map, returning")
         return
     try:
         with open(file, "r+", encoding="UTF-8") as f:
@@ -119,9 +190,27 @@ def parse_pylint_out(pylint_out):
         if line and line[0].isdigit() and 'fatal' not in line:
             num, err = line.split(":")
             if num in res:
-                res[num] = f"{res[num]},{err}"
+                if err not in res[num]:
+                    res[num] = f"{res[num]},{err}"
             else:
                 res[num] = err
+    return res
+
+def parse_pylint_out_module(pylint_out):
+    res = {}
+    for line in pylint_out.split("\n"):
+        line = line.strip()
+        if line and line[0] == '/':
+            file, num, err = line.split(":")
+            file, num, err = file.strip(), num.strip(), err.strip()
+            file_res = res.get(file,{})
+            if num in file_res:
+                if err not in file_res[num]:
+                    file_res[num] = f"{file_res[num]},{err}"
+            else:
+                if not res.get(file,{}):
+                    res[file] = {}
+                res[file][num] = err
     return res
 
 
